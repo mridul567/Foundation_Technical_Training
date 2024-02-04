20e2577776637c73713a2c03c55019e83273ac92
Mridul Bhardwaj <60420796+mridul567@users.noreply.github.com>
2024-02-04 22:23:02 +0530

    Add files via upload

---
+++ Python /Assignment-2/DBproperty.py
@@ -0,0 +1,7 @@
+class PropertyUtil:
+    def getPropertyString():
+        host = 'localhost'
+        username = 'root'
+        password = '9927559686'
+        database = 'sisdb'
+        return host, username, password, database
