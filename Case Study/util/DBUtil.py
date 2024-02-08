672b4e2d6c3a6209290c06a1cb21364f35d83a37
Mridul Bhardwaj <60420796+mridul567@users.noreply.github.com>
2024-02-08 09:40:30 +0530

    Add files via upload

---
+++ Case Study/util/DBUtil.py
@@ -0,0 +1,16 @@
+from mysql import connector
+import mysql
+
+
+class DBUtil:
+    @staticmethod
+    def dbconnection():
+        con = mysql.connector.connect(
+            host="localhost",
+            port="3306",
+            user="root",
+            password="9927559686",
+            database="carrentalsystem"
+        )
+        return con
+
