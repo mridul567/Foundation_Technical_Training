914bc9db27ef5e76e884ff47d1708a84d00f128d
Mridul Bhardwaj <60420796+mridul567@users.noreply.github.com>
2024-02-09 13:19:37 +0530

    Update payment.py

--- Case Study/entity/payment.py
+++ Case Study/entity/payment.py
@@ -4,3 +4,30 @@ class Payment:
         self.leaseID = leaseID
         self.paymentDate = paymentDate
         self.amount = amount
+
+    #Getter Methods
+    def getPaymentID(self):
+        return self.paymentID
+
+    def getLeaseID(self):
+        return self.leaseID
+
+    def getPaymentDate(self):
+        return self.paymentDate
+
+    def getAmount(self):
+        return self.amount
+
+    #Setter Methods
+    def setPaymentID(self,paymentID):
+        self.paymentID = paymentID
+
+    def setLeaseID(self,leaseID):
+        self.leaseID = leaseID
+
+    def setPaymentDate(self,paymentDate):
+        self.paymentDate = paymentDate
+
+    def setAmount(self,amount):
+        self.amount = amount
+
